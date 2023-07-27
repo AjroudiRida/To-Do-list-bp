fdb54bb1611948c791d7caee7cf668f04d6d03d9
unknown <kipkemoiedgah@gmail.com>
2023-07-27 17:53:13 +0300

    Applied unit test for clear all button

--- src/module/util.test.js
+++ src/module/util.test.js
@@ -3,6 +3,7 @@ import {
   removeTask,
   edit,
   statusUpdate,
+  clearAll,
 } from './util.js';
 
 describe('add and remove items', () => {
@@ -77,5 +78,29 @@ describe('edit, update completed status and clear All', () => {
       completed,
     } = tasks[0];
     expect(completed).toBeTruthy();
+  });
+  test('clear all', () => {
+    const data = [{
+      description: 'description',
+      completed: false,
+      index: 0,
+    }];
+    localStorage.setItem('taskList', JSON.stringify(data));
+    document.body.innerHTML = `
+    <div class='task-container'>
+      <div class='task'>
+          <label class="checkbox-container">
+            <input class="checkbox" type="checkbox" id="task-${data[0].index}" name="task" value="${data[0].description}" checked>
+            <span class="checkmark"></span>
+          </label>
+          <div class="content" contentEditable="true">
+            ${data[0].description}
+          </div>
+      </div>
+    </div>
+    `;
+    clearAll();
+    const tasks = document.querySelectorAll('.task-container .task');
+    expect(tasks).toHaveLength(0);
   });
 });
